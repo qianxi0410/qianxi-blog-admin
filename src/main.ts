15d83ac8cb9afc558b25e748b6663273df69aedd
qianxi0410 <894871277@qq.com>
2021-10-25 19:56:32 +0800

    chore: add mockjs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
+const { mockXHR } = require('../mock');
+mockXHR();
+
 Vue.config.productionTip = false;
 
 new Vue({
