d913fd514e0cb80a57270f01befa5a73faa3cc25
qianxi0410 <894871277@qq.com>
2021-10-29 15:42:07 +0800

    chore: type

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,27 @@ export interface UserInfo {
   username: string;
   password: string;
 }
+
+export interface Blog {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  title: string;
+  description: string;
+  pre: number;
+  next: number;
+  url: string;
+  path: string;
+  tags: string;
+}
+
+export interface Comment {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  content: string;
+  login: string;
+  name: string;
+  avatar: string;
+  postId: number;
+}
