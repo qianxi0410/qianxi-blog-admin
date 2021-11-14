de38fcfcf866a555b8acad082b143c071535c01b
qianxi0410 <894871277@qq.com>
2021-11-14 11:28:12 +0800

    Add 404 page and router

--- src/permission.ts
+++ src/permission.ts
@@ -27,10 +27,10 @@ router.beforeEach(async (to: Route, from: Route, next: NavigationGuardNext) => {
       next();
     }
   } else {
-    if (to.path !== '/login') {
-      next({ path: '/login' });
-    } else {
+    if (to.path === '/login' || to.path === '/404') {
       next();
+    } else {
+      next({ path: '/404' });
     }
   }
 });
